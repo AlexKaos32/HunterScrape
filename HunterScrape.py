231f28ab4a8fbf9b20a011fd57f9f0a7d1ff7af7
Alex <59406285+AlexKaos32@users.noreply.github.com>
2022-12-16 13:33:23 -0600

    Update HunterScrape.py
    Updated to remove usage of pyhunter library, as it does not return actual JSON data. Also implemented parsing of data to provide only emails.

--- HunterScrape.py
+++ HunterScrape.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 
-from pyhunter import PyHunter
+import requests
 import argparse
 import sys
+import json
+from operator import itemgetter
 
+#Using Argparse to specify cli arguments
 parser = argparse.ArgumentParser()
-
 parser.add_argument("-d", "--domain", type=str, required=True, help="Target domain to hunt")
 parser.add_argument("-k", "--key", type=str, required=True, help="Your hunter.io API key")
 args = parser.parse_args()
 
-hkey = PyHunter(args.key)
-scraped = hkey.domain_search(args.domain)
+#Request info from
+url = "https://api.hunter.io/v2/domain-search?domain="
+req = ''.join([url, args.domain, "&limit=100", "&api_key=", args.key])
+hunted = requests.get(req)
 
-sys.stdout = open('hunted.json', 'wt')
-print (scraped)
+#Output results to file to parse here, and further analysis
+with open ("hunted.json", "w") as f:
+    f.write((hunted.text))
+
+#Opening the output data
+huntFile = open('hunted.json','r')
+jdata = huntFile.read()
+
+#Parsing for emails
+obj=json.loads(jdata)
+entries = obj["data"]["emails"]
+
+bounty = list(map(itemgetter('value'), entries))
+
+print(str(bounty))
