039c204c4e8afa78ffcb732f006c9b4adda229d8
Alex <59406285+AlexKaos32@users.noreply.github.com>
2024-06-12 08:13:52 -0500

    Update HunterScrape.py

--- HunterScrape.py
+++ HunterScrape.py
@@ -12,6 +12,8 @@ args = parser.parse_args()
 
 url = "https://api.hunter.io/v2/domain-search?domain="
 
+#This is used to pull the number of emails in order to paginate in the hunter() func
+#However it's also used when the -l switch is used to specify a number of emails up to 100
 def total_check():
     global obj
     req = ''.join([url, args.domain, "&limit=", args.limit, "&api_key=", args.key])
@@ -20,18 +22,22 @@ def total_check():
     num_entries = obj["meta"]["results"]
     return num_entries
 
+#This builds the request for the hunter() function.
+#I could probably de-dup this and total_check(), but this is how it is for now
 def fetch_data(offset):
     global req
     req = ''.join([url, args.domain, "&limit=100","&offset=", str(offset), "&api_key=", args.key])
     fetched = requests.get(req)
     return fetched.json()
 
+#Initialize parameters
 def hunter():
     global obj
     total_results = total_check()
     all_emails = []
     offset = 0
 
+    #Loop through requests and extend the list
     while offset < total_check():
         data = fetch_data(offset)
         emails = [email['value'] for email in data["data"]["emails"]]
