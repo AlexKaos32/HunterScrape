823a3e53815da3d8a99abe22b0ea43d307831653
Alex <59406285+AlexKaos32@users.noreply.github.com>
2024-06-12 08:01:53 -0500

    V2 HunterScrape
    Changed the code up considerably to programmatically pull all results if desired.

--- HunterScrape.py
+++ HunterScrape.py
@@ -1,34 +1,52 @@
 #!/usr/bin/env python3
 
-import requests
-import argparse
-import sys
-import json
+import requests, argparse, sys, json
 from operator import itemgetter
 
-#Using Argparse to specify cli arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--domain", type=str, required=True, help="Target domain to hunt")
 parser.add_argument("-k", "--key", type=str, required=True, help="Your hunter.io API key")
+parser.add_argument("-l", "--limit", type=str, help="Number of emails to pull, limit 100" )
+parser.add_argument("-f", "--full", action='store_true', help="Toggle Full Results")
 args = parser.parse_args()
 
-#Request info from hunter using the API
 url = "https://api.hunter.io/v2/domain-search?domain="
-req = ''.join([url, args.domain, "&limit=100", "&api_key=", args.key])
-hunted = requests.get(req)
 
-#Output results to file to parse here, and further analysis if needed
-with open ("hunted.json", "w") as f:
-    f.write((hunted.text))
+def total_check():
+    global obj
+    req = ''.join([url, args.domain, "&limit=", args.limit, "&api_key=", args.key])
+    hunted = requests.get(req)
+    obj = json.loads(hunted.text)
+    num_entries = obj["meta"]["results"]
+    return num_entries
 
-#Opening the output data we just created
-huntFile = open('hunted.json','r')
-jdata = huntFile.read()
+def fetch_data(offset):
+    global req
+    req = ''.join([url, args.domain, "&limit=100","&offset=", str(offset), "&api_key=", args.key])
+    fetched = requests.get(req)
+    return fetched.json()
 
-#Parsing for emails
-obj=json.loads(jdata)
-entries = obj["data"]["emails"]
-bounty = list(map(itemgetter('value'), entries))
+def hunter():
+    global obj
+    total_results = total_check()
+    all_emails = []
+    offset = 0
 
-#Here's the loot!
-print(str(bounty))
+    while offset < total_check():
+        data = fetch_data(offset)
+        emails = [email['value'] for email in data["data"]["emails"]]
+        all_emails.extend(emails)
+        offset += 100
+
+    obj["emails"] = all_emails
+    return obj
+    
+
+if args.full:
+    hunter()
+else:
+    total_check()
+    obj["emails"] = [email['value'] for email in obj["data"]["emails"]]
+
+for email in obj["emails"]:
+    print(email)
